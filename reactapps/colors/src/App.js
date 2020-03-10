505ff9f42ae5f535e5057dbf4921dcd6e677e2a5
ashma27 <ashma.designoweb@gmail.com>
2020-03-10 23:42:39 +0530

    react project add

---
+++ reactapps/colors/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import purple from '@material-ui/core/colors/purple';
+import red from '@material-ui/core/colors/red';
+import pink from '@material-ui/core/colors/pink';
+import blue from '@material-ui/core/colors/blue';
+import indigo from '@material-ui/core/colors/indigo';
+import deepPurple from '@material-ui/core/colors/deepPurple';
+import teal from '@material-ui/core/colors/teal';
+import green from '@material-ui/core/colors/green';
+import orange from '@material-ui/core/colors/orange';
+import Colors from './colors';
+
+class App extends React.Component{
+  state = {
+    colors:[
+      purple[500],
+      purple[200],
+      purple[900],
+      red[500],
+      red[400],
+      red[900],
+      pink[500],
+      pink[200],
+      pink[900],
+      blue[900],
+      blue[500],
+      blue[200],
+      indigo[500],
+      indigo[200],
+      indigo[900],
+      deepPurple[500],
+      teal[500],
+      orange[500],
+      green[500]
+    ]
+  };
+  render(){
+    return <div className="App">
+    <Colors className={this.state.colors} />
+    hello
+    </div>;
+  }
+}
+
+export default App;
---
+++ reactapps/colors/src/colors.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+class Colors extends React.Component{
+    render(){
+    // return<div className="row">{this.props.colorNames.map}</div>;
+    }
+}
+
+export default Colors;
